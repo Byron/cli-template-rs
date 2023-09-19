812202a74c45d1a30f57e66220704923e8f736c6
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-09-19 11:04:03 +0200

    upgrade to clap 4

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
-#[macro_use]
-extern crate structopt;
-
 use anyhow::Context;
-use structopt::StructOpt;
+use clap::Parser;
 
 mod options {
     use std::path::PathBuf;
 
-    #[derive(Debug, StructOpt)]
+    #[derive(Debug, clap::Parser)]
     #[structopt(name = "example", about = "An example of StructOpt usage.")]
     pub struct Args {
         /// Activate debug mode
-        #[structopt(short = "d", long = "debug")]
+        #[clap(short = 'd', long)]
         pub debug: bool,
         /// Set speed
-        #[structopt(short = "s", long = "speed", default_value = "42")]
+        #[clap(short = 's', long, default_value = "42")]
         pub speed: f64,
         /// Input file
-        #[structopt(parse(from_os_str))]
         pub input: PathBuf,
         /// Output file, stdout if not present
-        #[structopt(parse(from_os_str))]
         pub output: Option<PathBuf>,
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let opt = options::Args::from_args();
+    let opt = options::Args::parse();
     println!("{:?}", opt);
     let path = opt.output.unwrap_or("foo.txt".into());
     foobar::fun(&path).with_context(|| format!("Could not handle file at path {}", path.display()))
